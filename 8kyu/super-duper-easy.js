12d3f5cb53dbf16a95787035dd9f06788569a624
Ahbideen Yusuf <ahbideenyusuf@gmail.com>
2023-07-17 14:59:53 +0100

    Added the first question

---
+++ 8kyu/super-duper-easy.js
@@ -0,0 +1,5 @@
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+function problem(x) {
+  //your code here
+}
