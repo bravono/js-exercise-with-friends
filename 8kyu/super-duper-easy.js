487e2aaee80a469795f6ca0ba8aeecdef68d6a6e
Monaj411 <100786191+Monaj411@users.noreply.github.com>
2023-07-18 18:12:20 +0100

    adding the fuction

--- 8kyu/super-duper-easy.js
+++ 8kyu/super-duper-easy.js
@@ -3,3 +3,13 @@
 function problem(x) {
   //your code here
 }
+
+// from my understanding of the code above, we're to write a funtion that'll increment the value of a given variable by 6 and it must return a number otherwise it should return an error message.
+
+function increment (value) {
+  value += 6
+  value *= 50
+  return value
+}
+
+console.log(increment(1));
