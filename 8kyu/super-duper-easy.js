a05dcb03a4307c72d27a514225c6a736e25d0584
Ahbideen Yusuf <ahbideenyusuf@gmail.com>
2023-07-18 19:24:22 +0100

    Added test cases

--- 8kyu/super-duper-easy.js
+++ 8kyu/super-duper-easy.js
@@ -1,5 +1,24 @@
+//QUESTION
 // Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
 
+// describe("Basic tests", () => {
+// it("Testing for fixed tests", () => {
+//   assert.strictEqual(problem("hello"), "Error");
+//   assert.strictEqual(problem(1), 56);
+//   assert.strictEqual(problem(5), 256);
+//   assert.strictEqual(problem(0), 6);
+//   assert.strictEqual(problem(1.2), 66);
+//   assert.strictEqual(problem(3), 156);
+//   assert.strictEqual(problem("RyanIsCool"), "Error");
+//   assert.strictEqual(problem(-3), -144);
+//   assert.strictEqual(problem(""), "Error");
+//   assert.strictEqual(problem(0.03), 7.5);
+// })
+// })
+
+// The question ask us to increment!
 function problem(x) {
-  //your code here
+  return x;
 }
+
+console.log(problem("Ahbideen"));
